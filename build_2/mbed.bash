52b36fc9a1a1694dc27d1818a8d60e5e4468f832
chikamori <>
2021-03-04 21:55:12 +0900

    add make command

---
+++ build_2/mbed.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+OBC="$(basename $1 .js)"
+
+if [ "$2" = "--32bit" ]; then
+  java -jar ejsc.jar --out-obc --out-bit32 $1 -o ${OBC}.obc > preload_strings.h
+else
+  java -jar ejsc.jar --out-obc $1 -o ${OBC}.obc > preload_strings.h
+fi
+od -v -t x1 ${OBC}.obc | sed -e 's/^[0-9][0-9]* */0x/' | sed -e 's/ *$/,/'| sed -e's/  */, 0x/g'| sed '$d' | sed '$s/,$//' > obc_contents.h
+
+make
